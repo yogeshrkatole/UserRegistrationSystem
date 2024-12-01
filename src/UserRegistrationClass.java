0440c367bd256fc0c3515e39091ed0d4e7102caf
yogeshrkatole@gmail.com <yogeshrkatole@gmail.com>
2024-12-01 20:56:40 +0530

    Check First Name

--- src/UserRegistrationClass.java
+++ src/UserRegistrationClass.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class UserRegistrationClass {
 	 public static void main(String args[]) {
 		 System.out.println("Welcome to User Registration System");
+		 
+		 Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter first name: "); 
+         String firstName = scanner.nextLine();
+
+         if (checkFirstName(firstName)) { 
+             System.out.println("First Name is Correct"); 
+         } else {
+             System.out.println("Incorrect\nFirst Name contains at least 3 char and starts with capital"); 
+         }
+
+         scanner.close();
+	 }
+	 public static boolean checkFirstName(String firstName) { 
+	     String regex = "^[A-Z][a-z]{2,}$";
+	     return Pattern.matches(regex, firstName);
 	 }
 }
