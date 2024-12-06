8bd7a2e8d9e0ba50550a064dd7be8259daa9ba81
yogeshrkatole@gmail.com <yogeshrkatole@gmail.com>
2024-12-06 16:21:50 +0530

    Write Parameterized Test For Multiple Emails Entry

--- src/test/java/UserRegistrationClassTest.java
+++ src/test/java/UserRegistrationClassTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import main.java.UserRegistrationClass;
 
@@ -58,12 +60,17 @@ public class UserRegistrationClassTest {
 		assertFalse(UserRegistrationClass.checkPassword("Katole"));
 	}
 
-	@Test
+	@ParameterizedTest
+	@ValueSource(strings = { "abc@yahoo.com", " abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+			"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" })
 	public void testSampleEmail_valid_returnHappy() {
 		assertTrue(UserRegistrationClass.checkSampleEmail("abc@yahoo.com"));
 	}
 
-	@Test
+	@ParameterizedTest
+	@ValueSource(strings = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com",
+			"abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com",
+			"abc@gmail.com.1a", "abc@gmail.com.aa.au" })
 	public void testSampleEmail_invalid_returnSad() {
 		assertFalse(UserRegistrationClass.checkSampleEmail("abc@.com.my"));
 	}
